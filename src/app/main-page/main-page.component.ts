bf6846efacd11905655f5ed29189692f4cae5d9f
Marcel Steffen <marcelsteffen@mac.speedport.ip>
2025-01-20 22:16:15 +0100

    skill section

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { SkillsComponent } from "./skills/skills.component";
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeroComponent, AboutMeComponent],
+  imports: [HeroComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
