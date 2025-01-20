15e44d3ba19533a12620acf4c0b09457bd5412a9
Marcel Steffen <marcelsteffen@mac.speedport.ip>
2025-01-21 00:39:40 +0100

    contact section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { FooterComponent } from "./shared/footer/footer.component";
+import { PortfolioComponent } from "./main-page/portfolio/portfolio.component";
+import { ReferenceComponent } from "./main-page/reference/reference.component";
+import { ContactComponent } from "./main-page/contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainPageComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, MainPageComponent, FooterComponent, PortfolioComponent, ReferenceComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "./hero/hero.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsComponent } from "./skills/skills.component";
+import { ReferenceComponent } from "./reference/reference.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeroComponent, AboutMeComponent, SkillsComponent],
+  imports: [HeroComponent, AboutMeComponent, SkillsComponent, ReferenceComponent, ContactComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
