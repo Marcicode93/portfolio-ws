76443beb93324a81d5cc32d884b1878f1c275511
Marcel Steffen <marcelsteffen@mac.speedport.ip>
2025-01-27 13:00:38 +0100

    contact reworked

--- src/app/main-page/contact/contact.component.ts
+++ src/app/main-page/contact/contact.component.ts
@@ -1,28 +1,56 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
   isChecked: boolean = false;
-  contactData ={
+  contactData = {
     name: '',
     email: '',
-    message: ''
+    message: '',
+  };
+
+  http= inject(HttpClient)
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://marcel-steffen.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  toggleCheckbox() {
+    this.isChecked = !this.isChecked;
   }
 
-  toggleCheckbox(){
-    this.isChecked = !this.isChecked;
-}
-
-
-onSubmit(){
-  console.log(this.contactData);
-  
-}
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+      ngForm.resetForm();
+    }
+  }
 }
